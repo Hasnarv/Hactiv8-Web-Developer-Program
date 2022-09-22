b5304ea12be5cff4514c74eacdb09ca51e7ff41e
Hasnarv <72425456+Hasnarv@users.noreply.github.com>
2022-09-22 20:40:06 +0700

    Sesi 8

---
+++ Live Seasson/Sesi 8/latihan1.js
@@ -0,0 +1,13 @@
+const siswa = {
+    nama : "Hasna",
+    prodi : "RPL",
+    smt : 7,
+    matkul : ["web programming", "alpro", "statistika"]
+}
+console.log(siswa)
+console.log(siswa.nama)
+console.log(siswa.prodi)
+console.log(siswa.matkul[1])
+siswa.matkul[1]="Algoritma Pemrograman"
+siswa.matkul[3]="Mobile Programming"
+console.log(siswa.matkul)
